6938292469c9d8265b05f9fba0ba6694a1085b20
dannytayade1495 <dannytayade1495@gmail.com>
2023-04-08 10:22:40 +0530

    first commit

---
+++ multithreading/src/com/jspiders/multithreading/main/ThreadMain.java
@@ -0,0 +1,12 @@
+package com.jspiders.multithreading.main;
+
+import com.jspiders.multithreading.threads.MyThread1;
+
+public class ThreadMain {
+	
+	public static void main(String[] args) {
+		MyThread1 myThread1 = new MyThread1();
+		myThread1.start();
+	}
+
+}
