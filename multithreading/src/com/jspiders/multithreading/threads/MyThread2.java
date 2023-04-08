3708bc4ae23efdf4a197d9ac7f8930ba47656cca
dannytayade1495 <dannytayade1495@gmail.com>
2023-04-08 11:58:39 +0530

    second commit

--- multithreading/src/com/jspiders/multithreading/threads/MyThread2.java
+++ multithreading/src/com/jspiders/multithreading/threads/MyThread2.java
@@ -1,11 +1,11 @@
 package com.jspiders.multithreading.threads;
 
-public class MyThread1 extends Thread {
+public class MyThread2 implements Runnable {
 
 	@Override
 	public void run() {
 		for (int i = 1; i <= 5; i++) {
-			System.out.println("MyThread 1 is now running..!!");
+			System.out.println("MyThread 2 is now running..!!");
 		}
 	}
 
