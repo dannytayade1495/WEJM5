7adc30ff609b95c37783d2b7ae90234764ad30a8
dannytayade1495 <dannytayade1495@gmail.com>
2023-05-22 15:21:11 +0530

    changes till 22-05-23

--- hibernate/src/main/java/com/jspiders/hibernate/dao/StudentDAO.java
+++ hibernate/src/main/java/com/jspiders/hibernate/dao/StudentDAO.java
@@ -1,5 +1,46 @@
 package com.jspiders.hibernate.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.jspiders.hibernate.dto.StudentDTO;
+
 public class StudentDAO {
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory entityManagerFactory = 
+				Persistence.createEntityManagerFactory("hibernate");
+		EntityManager entityManager = 
+				entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = 
+				entityManager.getTransaction();
+		
+		entityTransaction.begin();
+		
+		StudentDTO student1 = new StudentDTO();
+		student1.setSid(1);
+		student1.setSname("Shivam");
+		student1.setSemail("shivam@gmail.com");
+		student1.setSmarks(70);
+		student1.setSadd("Pune");
+		
+		entityManager.persist(student1);
+		
+		entityTransaction.commit();
+		
+		if (entityManagerFactory != null) {
+			entityManagerFactory.close();
+		}
+		if (entityManager != null) {
+			entityManager.close();
+		}
+		if (entityTransaction.isActive()) {
+			entityTransaction.rollback();
+		}
+		
+	}
 
 }
--- hibernate/src/main/java/com/jspiders/hibernate/dto/StudentDTO.java
+++ hibernate/src/main/java/com/jspiders/hibernate/dto/StudentDTO.java
@@ -1,5 +1,19 @@
 package com.jspiders.hibernate.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class StudentDTO {
+	
+	@Id
+	private int sid;
+	private String sname;
+	private String semail;
+	private int smarks;
+	private String sadd;
 
 }
