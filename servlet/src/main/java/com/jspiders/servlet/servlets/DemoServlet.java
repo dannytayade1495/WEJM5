4b590c58ed69aa7b3684d62e01d4fbfadc4097ab
dannytayade1495 <dannytayade1495@gmail.com>
2023-06-02 10:28:35 +0530

    changes till 01-06-23

---
+++ servlet/src/main/java/com/jspiders/servlet/servlets/DemoServlet.java
@@ -0,0 +1,30 @@
+package com.jspiders.servlet.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/demo")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, 
+						HttpServletResponse response) 
+						throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello from DemoServlet</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, 
+						HttpServletResponse response) 
+						throws ServletException, IOException {
+	}
+
+}
